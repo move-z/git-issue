028267d0f533d85a7e7187a22364ae28d52b8bfc
Rodolfo Boer <rodolfo.boer@gmail.com>
2022-08-12 17:41:56 +0200

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,26 @@
-use crate::arguments::Args;
-use anyhow::Result;
+use anyhow::{Result, bail};
 use clap::Parser;
+
+use arguments::Args;
 
 mod arguments;
 mod git;
+mod github;
 mod jira;
 
 fn main() -> Result<()> {
     let args = Args::parse();
+    let id = args.id;
 
-    if args.config {
-        eprintln!("config is TODO!!!");
-    } else if let Some(id) = args.id {
-        jira::handle(&id)?;
-    }
+    let kind = git::get_config("personality")?;
+
+    let issue = match kind.as_str() {
+        "github" => github::get_issue_title(&id)?,
+        "jira" => jira::get_issue_title(&id)?,
+        _ => bail!("{kind} is not a valid value for personality\nvalid values: github, jira"),
+    };
+
+    dbg!(issue);
 
     Ok(())
 }

222384af1cc63e318ea639e005d906dacbd51585
Rodolfo Boer <rodolfo.boer@gmail.com>
2023-03-23 11:29:22 +0100

    don't escape characters in the commit message - #36 cleaned up code

--- src/utils.rs
+++ src/utils.rs
@@ -1,7 +1,19 @@
 use anyhow::Result;
+use regex::Regex;
 
 /// Ask for password
 pub fn ask_password(prompt: &str) -> Result<String> {
     let p = rpassword::prompt_password(format!("Enter password for {prompt}: "))?;
     Ok(p)
 }
+
+pub fn escape_branch_name(input: &str) -> String {
+    let res = Regex::new(r"[\p{Space Separator}/]").unwrap().replace_all(input, "-");
+    // partially implemented according to
+    // https://git-scm.com/docs/git-check-ref-format
+    let res = Regex::new(r"[\p{Control},~^:?*\[\]\\@{}]").unwrap().replace_all(&res, "");
+    let res = Regex::new(r"\.\.+").unwrap().replace_all(&res, ".");
+    let res = Regex::new(r"--+").unwrap().replace_all(&res, "-");
+    res.into_owned()
+}
+
